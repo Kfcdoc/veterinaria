e7e0bf35d83cb823b7b38c2ed37a50adf8765b93
Kfcdoc <cruzhernandezisay@gmail.com>
2026-05-16 19:41:49 -0600

    Refactor: extrae script JS del formulario create a archivo externo

--- resources/views/modules/admin/usuarios/create.blade.php
+++ resources/views/modules/admin/usuarios/create.blade.php
@@ -80,24 +80,5 @@
 @endsection
 
 @push('scripts')
-<script>
-    function toggleVetFields() {
-        var rol = document.getElementById('rol').value;
-        var vetFields = document.getElementById('veterinario_fields');
-        var cedulaInput = document.getElementById('cedula_profesional');
-        
-        if (rol === 'veterinario') {
-            vetFields.style.display = 'block';
-            cedulaInput.required = true;
-        } else {
-            vetFields.style.display = 'none';
-            cedulaInput.required = false;
-        }
-    }
-
-    // Ejecutar al cargar la página por si hay errores de validación y se mantuvo la opción seleccionada
-    window.onload = function() {
-        toggleVetFields();
-    };
-</script>
+<script src="{{ asset('startbootstrap/js/admin/usuarios/create.js') }}"></script>
 @endpush
